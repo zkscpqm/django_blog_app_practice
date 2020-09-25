bd2fe40cbbc1deca28d80b3a846bfcc6ff1c1030
Georgi <zkscpqm@daum.net>
2020-09-25 22:37:28 +0300

    db setup + added and registered blog object

--- practice_project/blog/models.py
+++ practice_project/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class BlogPost(models.Model):
+
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(default=timezone.now)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title}-{self.author}"
--- practice_project/blog/views.py
+++ practice_project/blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        'author': 'Georgi',
-        'title': 'first post',
-        'content': 'lorem ipsum',
-        'date_posted': '27-01-2020'
-    },
-    {
-        'author': 'Vanya',
-        'title': 'second post',
-        'content': 'lorem ipsum 2',
-        'date_posted': '21-03-2020'
-    }
-]
+from .models import BlogPost
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': BlogPost.objects.all()
     }
     return render(request, 'blog/home.html', context=context)
 
