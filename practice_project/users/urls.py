164216d3ec6165932f831ca408c4464fb01b25cd
Georgi <zkscpqm@daum.net>
2020-09-26 17:53:02 +0300

    implemented login, logout, and linking/routing

--- practice_project/users/urls.py
+++ practice_project/users/urls.py
@@ -1,6 +1,10 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.register, name='blog-register'),
+    path('register/', views.register, name='blog-register'),
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='blog-login'),
+    path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='blog-logout'),
+    path('profile/', views.profile, name='user-profile'),
 ]
--- practice_project/users/views.py
+++ practice_project/users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from django.shortcuts import render
 from .forms import UserRegisterForm
@@ -10,12 +11,16 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}.')
-            return redirect('blog-home')
+            messages.success(request, f'Account created. You can now log in..')
+            return redirect('blog-login')
         messages.warning(request, f'Account registration failed.')
     else:
         form = UserRegisterForm()
 
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
 
