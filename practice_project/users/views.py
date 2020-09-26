e4b621f189c6d5ac1aa05792524d3d640370af60
Georgi <zkscpqm@daum.net>
2020-09-26 13:59:56 +0300

    linked registration to button + fixed error msg

--- practice_project/users/views.py
+++ practice_project/users/views.py
@@ -13,8 +13,9 @@ def register(request):
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}.')
             return redirect('blog-home')
+        messages.warning(request, f'Account registration failed.')
     else:
         form = UserRegisterForm()
-        messages.warning(request, f'Account registration failed.')
+
     return render(request, 'users/register.html', {'form': form})
 
