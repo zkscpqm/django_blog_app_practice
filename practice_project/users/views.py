599ceaef39ae1486846cbc045108a6d520c9925f
Georgi <zkscpqm@daum.net>
2020-09-26 13:53:59 +0300

    updated rego form to include email + crispy

--- practice_project/users/views.py
+++ practice_project/users/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import redirect
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
 from django.contrib import messages
 
 
 def register(request):
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}.')
             return redirect('blog-home')
     else:
-        form = UserCreationForm()
-
+        form = UserRegisterForm()
+        messages.warning(request, f'Account registration failed.')
     return render(request, 'users/register.html', {'form': form})
 
