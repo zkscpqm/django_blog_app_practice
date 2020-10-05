f1ac5f5d75211d46f6d795e6adac6b9f41eb252b
Georgi <zkscpqm@daum.net>
2020-10-05 09:01:37 +0300

    removed secret key

--- practice_project/practice_project/settings.py
+++ practice_project/practice_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9yab=4m(r#=11+9@dhtk)azurne3gjudi*k1*x4vk&0_t=1!+#'
+SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
